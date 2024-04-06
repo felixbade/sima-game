1caa1507fbecd983129b9d4b2c61ef3abe6a7b32
Felix Bade <flix.bade@gmail.com>
2024-04-06 16:40:32 +0300

    Fix bg re-rendering lag

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -7,22 +7,13 @@ window.addEventListener('load', () => {
 
     // brown background
     const bg = new PIXI.Graphics();
-    container.addChild(bg);
+    // ok this lagged too much when it was in ticker. but it will not resize now
+    bg.beginFill(0xd99c38);
+    const aspect = app.screen.width / app.screen.height;
+    bg.drawRect(-aspect * 500, -500, aspect * 1000, 1000);
+    bg.endFill();
 
-    // todo: sima bubble
-    const particle = PIXI.Sprite.from('assets/images/gas-particle.svg');
-    particle.anchor.set(0.5);
-    particle.scale.set(0.5); // 2x dpi
-    gameContainer.addChild(particle);
-
-    app.ticker.add(delta => {
-        // needed here for resize
-        // rgb(217, 156, 56)
-        bg.beginFill(0xd99c38);
-        const aspect = app.screen.width / app.screen.height;
-        bg.drawRect(-aspect * 500, -500, aspect * 1000, 1000);
-        bg.endFill();
-
+    gameContainer.addChild(bg);
 
     // todo: sima bubble
     const bubbleTexture = PIXI.Texture.from('assets/images/bubble.png');
@@ -31,6 +22,8 @@ window.addEventListener('load', () => {
     bubble.anchor.set(0.5);
     bubble.scale.set(0.5); // 2x dpi
     gameContainer.addChild(bubble);
+
+    app.ticker.add(delta => {
         const speed = 5;
         bubble.x += controller.move.x * delta * speed;
         bubble.y += controller.move.y * delta * speed;
