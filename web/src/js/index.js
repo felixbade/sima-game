5f640408b404c016a674f0babd4bb25a5a53aee3
Felix Bade <flix.bade@gmail.com>
2024-04-06 18:36:57 +0300

    Bubbles eat each other

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -25,39 +25,81 @@ window.addEventListener('load', () => {
         const bubble = new PIXI.Sprite(bubbleTexture);
         bubble.anchor.set(0.5);
         bubble.scale.set(0.5 * size / 100); // 2x dpi
+        bubble.size = size;
         bubble.x = x;
         bubble.y = y;
+        bubble.vx = 0;
+        bubble.vy = 0;
         gameContainer.addChild(bubble);
         bubbles.push(bubble);
     }
 
     // bubbles[0] is the player
-    newBubble(0, 0, 30);
+    newBubble(0, 0, 10000);
 
-    for (let i = 0; i < 500; i++) {
-        const minSize = 10;
-        const maxSize = 50;
+    for (let i = 0; i < 1000; i++) {
+        const minSize = 50;
+        const maxSize = 5000;
         const size = Math.exp(Math.random() * Math.log(maxSize / minSize) + Math.log(minSize));
-        const x = Math.random() * 1000 - 500;
-        const y = Math.random() * 1000 - 500;
+        const x = Math.random() * 5000 - 2500;
+        const y = 500 - Math.random() * 10000;
         newBubble(x, y, size);
     }
 
-    app.ticker.add(delta => {
+    app.ticker.add(dt => {
         const speed = 5;
-        bubbles[0].x += controller.move.x * delta * speed;
-        bubbles[0].y += controller.move.y * delta * speed;
+        bubbles[0].x += controller.move.x * dt * speed;
+        bubbles[0].y += controller.move.y * dt * speed;
 
-        for (let i = 1; i < bubbles.length; i++) {
-            const time = app.ticker.lastTime / 1000 * 0.2;
-            const noiseX = bubbles[i].x / 300;
-            const noiseY = bubbles[i].y / 300;
-            const delta = 0.001;
-            const speed = 0.1;
-            const dx = noise(noiseX + delta, noiseY, time) - noise(noiseX, noiseY, time);
-            const dy = noise(noiseX, noiseY + delta, time) - noise(noiseX, noiseY, time);
-            bubbles[i].x += dx / delta * speed;
-            bubbles[i].y += dy / delta * speed;
+        for (let i = 0; i < bubbles.length; i++) {
+            // random floating in the field
+            const time = app.ticker.lastTime / 1000 * 0.3;
+            const noiseX = bubbles[i].x / 500;
+            const noiseY = bubbles[i].y / 500;
+            const noiseDelta = 0.001;
+            const drag = 3 / bubbles[i].size;
+            const current = 0.1;
+            let vx = noise(noiseX + noiseDelta, noiseY, time) - noise(noiseX, noiseY, time);
+            let vy = noise(noiseX, noiseY + noiseDelta, time) - noise(noiseX, noiseY, time);
+            vx *= current / noiseDelta;
+            vy *= current / noiseDelta;
+
+            const dvx = bubbles[i].vx - vx;
+            const dvy = bubbles[i].vy - vy;
+            bubbles[i].vx -= dvx * dt * drag;
+            bubbles[i].vy -= dvy * dt * drag;
+        }
+
+        // bubbles that are touching get merged
+        for (let i = 0; i < bubbles.length; i++) {
+            for (let j = i + 1; j < bubbles.length; j++) {
+                const dx = bubbles[i].x - bubbles[j].x;
+                const dy = bubbles[i].y - bubbles[j].y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+                const overlap = bubbles[i].radius + bubbles[j].radius - distance;
+                if (overlap > 0) {
+                    const smaller = Math.min(bubbles[i].size, bubbles[j].size);
+                    const eatR = Math.min(overlap, smaller);
+                    const eat = Math.pow(eatR, 2);
+
+                    // bigger eats smaller, slowly
+                    if (bubbles[i].size > bubbles[j].size) {
+                        bubbles[i].size += eat;
+                        bubbles[j].size -= eat;
+                    } else {
+                        bubbles[j].size += eat;
+                        bubbles[i].size -= eat;
+                    }
+
+                }
+            }
+        }
+
+        for (let i = 0; i < bubbles.length; i++) {
+            bubbles[i].x += bubbles[i].vx * dt;
+            bubbles[i].y += bubbles[i].vy * dt;
+            bubbles[i].radius = Math.sqrt(bubbles[i].size) / 2;
+            bubbles[i].scale.set(2 * bubbles[i].radius / 240); // 240px size bubble
         }
 
         // if trigger is pressed, move player to the center
